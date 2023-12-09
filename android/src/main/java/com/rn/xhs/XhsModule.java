6fb7950bec6e6038880d5e9162a7b1f78769f195
liuqiang <liuqiang@cfhy.com>
2023-12-09 17:25:19 +0800

    feat: android 分享

--- android/src/main/java/com/rn/xhs/XhsModule.java
+++ android/src/main/java/com/rn/xhs/XhsModule.java
@@ -3,20 +3,25 @@
 package com.rn.xhs;
 
 import android.net.Uri;
+import android.util.Log;
 import android.webkit.URLUtil;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
+import com.xingin.xhssharesdk.callback.XhsShareCallback;
 import com.xingin.xhssharesdk.callback.XhsShareRegisterCallback;
 import com.xingin.xhssharesdk.core.XhsShareSdk;
 import com.xingin.xhssharesdk.model.config.XhsShareGlobalConfig;
 import com.xingin.xhssharesdk.model.sharedata.XhsImageInfo;
 import com.xingin.xhssharesdk.model.sharedata.XhsImageResourceBean;
 import com.xingin.xhssharesdk.model.sharedata.XhsNote;
+import com.xingin.xhssharesdk.model.sharedata.XhsVideoInfo;
+import com.xingin.xhssharesdk.model.sharedata.XhsVideoResourceBean;
 
 import java.io.File;
 import java.lang.reflect.Array;
@@ -26,6 +31,7 @@ import java.util.Arrays;
 public class XhsModule extends ReactContextBaseJavaModule {
 
     private final ReactApplicationContext reactContext;
+    private String TAG ="XhsModule";
 
     public XhsModule(ReactApplicationContext reactContext) {
         super(reactContext);
@@ -51,11 +57,14 @@ public class XhsModule extends ReactContextBaseJavaModule {
         XhsShareSdk.registerApp(reactContext, appkey, config, new XhsShareRegisterCallback() {
             @Override
             public void onSuccess() {
+                Log.e(TAG,"register: onSuccess");
+
                 callback.invoke("success");
             }
 
             @Override
             public void onError(int i, String s, @Nullable Exception e) {
+                Log.e(TAG,"register: onError"+s+e.getMessage());
                 callback.invoke("error:" + e.getMessage());
 
             }
@@ -63,14 +72,59 @@ public class XhsModule extends ReactContextBaseJavaModule {
     }
 
     @ReactMethod
-    public void shareImage(String title, String content, String imageUrl, Callback callback) {
+    public void shareImage(String title, String content, String imageUrl) {
         XhsNote xhsNote = new XhsNote();
         xhsNote.setTitle(title);
         xhsNote.setContent(content);
         xhsNote.setImageInfo(new XhsImageInfo(Arrays.asList(
-                new XhsImageResourceBean(new File(imageUrl))))
+                XhsImageResourceBean.fromUrl(imageUrl)
+                ))
         );
 
         String sessionId = XhsShareSdk.shareNote(reactContext.getCurrentActivity(), xhsNote);
+        Log.e(TAG,"sessionId:"+sessionId);
+
+        XhsShareSdk.setShareCallback(new XhsShareCallback() {
+            @Override
+            public void onSuccess(String s) {
+                Log.e(TAG,"setShareCallback onSuccess: "+s);
+            }
+
+            @Override
+            public void onError(@NonNull String s, int i, @NonNull String s1, @Nullable Throwable throwable) {
+                Log.e(TAG,"setShareCallback onError: "+s);
+                Log.e(TAG,"setShareCallback onError: "+s1);
+                Log.e(TAG,"setShareCallback onError: "+throwable.getMessage());
+            }
+        });
+    }
+
+    @ReactMethod
+    public void shareVideo(String title, String content,String imageUrl, String videoUrl) {
+        XhsNote xhsNote = new XhsNote();
+        xhsNote.setTitle(title);
+        xhsNote.setContent(content);
+        xhsNote.setVideoInfo(new XhsVideoInfo(
+                XhsVideoResourceBean.fromUrl(videoUrl),
+                        XhsImageResourceBean.fromUrl(imageUrl)
+                )
+        );
+
+        String sessionId = XhsShareSdk.shareNote(reactContext.getCurrentActivity(), xhsNote);
+        Log.e(TAG,"sessionId:"+sessionId);
+
+        XhsShareSdk.setShareCallback(new XhsShareCallback() {
+            @Override
+            public void onSuccess(String s) {
+                Log.e(TAG,"setShareCallback onSuccess: "+s);
+            }
+
+            @Override
+            public void onError(@NonNull String s, int i, @NonNull String s1, @Nullable Throwable throwable) {
+                Log.e(TAG,"setShareCallback onError: "+s);
+                Log.e(TAG,"setShareCallback onError: "+s1);
+                Log.e(TAG,"setShareCallback onError: "+throwable.getMessage());
+            }
+        });
     }
 }
--- index.ts
+++ index.ts
@@ -8,6 +8,14 @@ export default Xhs;
 
 export function initAMapSearch(key:string){
     Xhs.register(key,(res:any)=>{
-        console.log('result:',res)
+        console.log('initAMapSearch result:',res)
     });
+}
+
+export function shareImage(title:string,content:string,imageUrl:string){
+    Xhs.shareImage(title,content,imageUrl);
+}
+
+export function shareVideo(title:string,content:string, imageUrl:string,videoUrl:string){
+    Xhs.shareVideo(title,content,imageUrl,videoUrl);
 }
