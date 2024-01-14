a8dbe96eeae9b5ee0e8a80e707ddff0fa6954e54
zhoukai <zhoukai@bituniverse.org>
2024-01-14 21:32:57 +0800

    feat: ios 支持分享多个图片

--- ios/Xhs.m
+++ ios/Xhs.m
@@ -52,6 +52,37 @@ RCT_EXPORT_METHOD(shareImage:(NSString *)title :(NSString *)content :(NSString *
 
 }
 
+RCT_EXPORT_METHOD(shareImages:(NSString *)title :(NSString *)content :(NSArray *)imageUrls :(RCTResponseSenderBlock)callback) {
+
+    dispatch_async(dispatch_get_main_queue(), ^{
+
+        XHSOpenSDKShareRequest *shareRequest = [[XHSOpenSDKShareRequest alloc] init];
+        shareRequest.mediaType = XHSOpenSDKShareMediaTypeImage;
+        NSMutableArray<XHSShareInfoImageItem *> *imageResources = [NSMutableArray array];
+        XHSShareInfoImageItem *imageObject = [[XHSShareInfoImageItem alloc] init];
+        /// 图片远程url 或者本地沙盒路径
+        for (NSString *urlString in imageUrls) {
+            XHSShareInfoImageItem *imageObject = [[XHSShareInfoImageItem alloc] init];
+            imageObject.imageUrl = urlString; // 假定 XHSShareInfoImageItem 有一个 imageUrl 属性
+            [imageResources addObject:imageObject];
+        }
+        XHSShareInfoTextContentItem * textItem = [[XHSShareInfoTextContentItem alloc]init];
+        textItem.title = title;
+        textItem.content = content;
+        shareRequest.textContentItem = textItem;
+        shareRequest.imageInfoItems = imageResources;
+        [XHSApi sendRequest:shareRequest completion:^(BOOL success) {
+            if (success) {
+                callback(@[@"success"]);
+            }else{
+                callback(@[@"error"]);
+            }
+
+        }];
+
+    });
+
+}
 
 RCT_EXPORT_METHOD(shareVideo:(NSString *)title :(NSString *)content :(NSString *)imageUrl :(NSString *)videoUrl :(RCTResponseSenderBlock)callback) {
 
